a8e0c2312e767e537dbcbe6a1ba7863500b8e678
Акулич Павел Александрович <akulich@unibel.by>
2020-08-20 09:11:57 +0300

    aaa

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => {
+    return <header className="header">
+        <img src="https://im0-tub-by.yandex.net/i?id=0caa159bede0a70e1eb0455e38769dc9&n=13"></img>
+      </header>
+}
+
+export default Header;
